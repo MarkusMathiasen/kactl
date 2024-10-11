a44b2b11aa85148ef9cebaeec40e5478875c411e
Markus Mathiasen <markusemathiasen@gmail.com>
2024-10-11 15:35:16 +0200

    Add Alignment

--- content/strings/Alignment.h
+++ content/strings/Alignment.h
@@ -1,6 +1,6 @@
 /**
  * Author: Markus Mathiasen
- * License: UPL
+ * License: CC0
  * Source: Competetive Programming 4 (book 2)
  * Description: Given two vectors, it will compute the alignment of the vectors, which maximizes the score.
  * This can be used to find the "edit distance". Insert and delete cost refers to inserting and deleting from A.
@@ -12,14 +12,14 @@
 
 template<class T>
 ll alignment(vector<T>& A, vector<T>& B, vector<vi>& dp) {
+    ll del=-1,ins=-1,match=2,mismatch=-1;
     dp.assign(sz(A)+1, vi(sz(B)+1, 0));
-    rep(i, 0, sz(A)+1) dp[i][0] = i*(-1); // delete score -1
-    rep(j, 1, sz(B)+1) dp[0][j] = j*(-1); // insert score -1
+    rep(i, 0, sz(A)+1) dp[i][0] = i*del;
+    rep(j, 1, sz(B)+1) dp[0][j] = j*ins;
     rep(i, 1, sz(A)+1) rep(j, 1, sz(B)+1) {
-        // match and mismatch score (2 and -1)
-        dp[i][j]=dp[i-1][j-1]+(A[i-1]==B[j-1]?2:-1);
-        dp[i][j]=max(dp[i][j],dp[i-1][j]-1); // delete -1
-        dp[i][j]=max(dp[i][j],dp[i][j-1]-1); // insert -1
+        dp[i][j]=dp[i-1][j-1]+(A[i-1]==B[j-1]?match:mismatch);
+        dp[i][j]=max(dp[i][j],dp[i-1][j]+del);
+        dp[i][j]=max(dp[i][j],dp[i][j-1]+ins);
     }
     return dp[sz(A)][sz(B)];
 }
