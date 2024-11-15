f4e6a28f79a7301dabd015c6ee9cbd3521efce15
Markus Mathiasen <markusemathiasen@gmail.com>
2024-11-15 09:09:39 +0100

    Remove line-wraps by making lines shorter

--- content/various/HuntSzymanski.h
+++ content/various/HuntSzymanski.h
@@ -12,18 +12,18 @@
 #pragma once
 
 template<class T> vector<T> lcs(vector<T>& A, vector<T>& B) {
-    unordered_map<T, vi> rev; vector<vi> prev(sz(A));
-    vector<vi> dp; rep(i, 0, sz(B)) rev[B[i]].push_back(i);
-    rep(i, 0, sz(A)) prev[i].resize(sz(rev[A[i]]));
-    rep(i, 0, sz(A)) for (int j = sz(rev[A[i]])-1; j >= 0; j--) {
-        int y = rev[A[i]][j];
-        auto it = lower_bound(all(dp), vi{y, 0, 0});
-        if (it == dp.end()) dp.emplace_back(), it = dp.end()-1;
-        *it={y,i,j};
-        if(it!=dp.begin())prev[i][j]=(*(it-1))[1]*sz(B)+(*(it-1))[2];
-    }
-    if (dp.empty()) {return {};} int L=sz(dp);
-    vector<T> res(L); int c=dp.back()[1]*sz(B)+dp.back()[2];
+	unordered_map<T, vi> rev; vector<vi> prev(sz(A));
+	vector<vi> dp; rep(i, 0, sz(B)) rev[B[i]].push_back(i);
+	rep(i, 0, sz(A)) prev[i].resize(sz(rev[A[i]]));
+	rep(i, 0, sz(A)) for (int j = sz(rev[A[i]])-1; j >= 0; j--) {
+		int y = rev[A[i]][j];
+		auto it = lower_bound(all(dp), vi{y, 0, 0});
+		if (it == dp.end()) dp.emplace_back(), it = dp.end()-1;
+		*it={y,i,j};
+		if(it!=dp.begin())it--,prev[i][j]=(*it)[1]*sz(B)+(*it)[2];
+	}
+	if (dp.empty()) {return {};} int L=sz(dp);
+	vector<T> res(L); int c=dp.back()[1]*sz(B)+dp.back()[2];
 	while(L--)res[L]=A[c/sz(B)],c=prev[c/sz(B)][c%sz(B)];
 	return res;
 }
